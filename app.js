72e56c449ec7f665b8d3b4d53df4c5372eee1f6b
chaoreum <chaoreum0525@gmail.com>
2022-02-09 17:22:54 +0900

    Backup File-1

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-const a = 5;
-const b = 2;
-const myName = "oreumi";
+// const a = 5;
+// const b = 2;
+// let myName = "oreumi"; //string
 
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
-console.log("hello " + myName);
+// //let - const : const(Constant(상수)=값이 바뀔 수 없음.)
+// //let으로 myName을 설정하면 후에 업데이트 가능,
+// //const로 설정하면 업데이트 안되고 에러가 뜸.
+// //always = const, sometimes = let, never = var
+
+// console.log(a + b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log("hello " + myName);
+
+// myName = "chaoreum";
+
+// console.log("your new name is " + myName);
+
+// Boolean //true or False
+const amIFat = null; //null means "NOTHING in there". exist has a value, but the value is "NOTHING"
+let something; // exist doesn't have any value.
+console.log(something, amIFat);
